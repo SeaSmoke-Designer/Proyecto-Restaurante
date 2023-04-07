be624f3fd484a885126020cbd934204835cd4065
IAN ESTEBAN TAUZY LAMAGNA <iantaulam@alu.edu.gva.es>
2023-04-07 20:22:50 +0200

    Pequeños cambios

--- Proyecto-Restaurante/App.xaml.cs
+++ Proyecto-Restaurante/App.xaml.cs
@@ -13,5 +13,9 @@ namespace Proyecto_Restaurante
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(Proyecto_Restaurante.Properties.Settings.Default.ClaveSyncfusion);
+        }
     }
 }
---
+++ Proyecto-Restaurante/VistasModelo/MainWindowVM.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Proyecto_Restaurante.VistasModelo
+{
+    class MainWindowVM : ObservableObject
+    {
+        private UserControl contenidoVentana;
+        public UserControl ContenidoVentana
+        {
+            get { return contenidoVentana; }
+            set { SetProperty(ref contenidoVentana, value); }
+        }
+
+
+
+    }
+}
